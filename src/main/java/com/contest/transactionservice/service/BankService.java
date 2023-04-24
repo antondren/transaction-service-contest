279c8e48d369af01c4169bff6f7162a68218b35e
Anton Drehalo <adrehalo@match-trade.com>
2023-04-24 10:24:28 +0200

    updateDebitAccount and updateCreditAccount functions created

--- src/main/java/com/contest/transactionservice/service/BankService.java
+++ src/main/java/com/contest/transactionservice/service/BankService.java
@@ -6,6 +6,7 @@ import com.contest.transactionservice.dto.Transaction;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Comparator;
 import java.util.List;
 
@@ -24,11 +25,8 @@ public class BankService {
             Account debitAccount = repository.getAccount(transaction.debitAccount());
             Account creditAccount = repository.getAccount(transaction.creditAccount());
 
-            debitAccount.setDebitCount(debitAccount.getDebitCount() + 1);
-            debitAccount.setBalance(debitAccount.getBalance().subtract(transaction.amount()));
-
-            creditAccount.setCreditCount(creditAccount.getCreditCount() + 1);
-            creditAccount.setBalance(creditAccount.getBalance().add(transaction.amount()));
+            updateDebitAccount(debitAccount, transaction.amount());
+            updateCreditAccount(creditAccount, transaction.amount());
 
             repository.saveAccountInformation(debitAccount);
             repository.saveAccountInformation(creditAccount);
@@ -39,5 +37,15 @@ public class BankService {
         List<Account> sortedAccounts = repository.getAllAccounts();
         sortedAccounts.sort(Comparator.comparing(Account::getAccountNumber));
         return sortedAccounts;
+    }
+
+    private void updateDebitAccount(Account debitAccount, BigDecimal amount) {
+        debitAccount.setDebitCount(debitAccount.getDebitCount() + 1);
+        debitAccount.setBalance(debitAccount.getBalance().subtract(amount));
+    }
+
+    private void updateCreditAccount(Account creditAccount, BigDecimal amount) {
+        creditAccount.setCreditCount(creditAccount.getCreditCount() + 1);
+        creditAccount.setBalance(creditAccount.getBalance().add(amount));
     }
 }
